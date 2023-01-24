380118c4df991b7716f1629894ba30885d6d76b7
Marcos Vinicius <marcao.vfo@gmail.com>
2023-01-24 08:46:13 -0400

    fix: fix on code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
+
+import "./global.css";
 
 function App() {
 
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+
+      <Tasks />
     </div>
   )
 }
